7a6d3532d6342d01d1000a09d613b83d03eccdcd
Yosef781227 <yosefnakachew101@gmail.com>
2023-04-25 14:29:17 +0300

    final touch

--- src/content_option.js
+++ src/content_option.js
@@ -1,162 +1,179 @@
 const logotext = "Yosef";
 const meta = {
-    title: "Yosef Degarge",
-    description: "I’m Yosef Degarge  Frontend devloper,currently working in Ethiopia",
+  title: "Yosef Degarge",
+  description:
+    "I’m Yosef Degarge  Frontend devloper,currently working in Ethiopia",
 };
 
 const introdata = {
-    title: "I’m Yosef Degarge",
-    animated: {
-        first: "I love coding",
-        second: "I code Awsome websites",
-        third: "I am Frontend Developer",
-    },
-    description: "I am a solution oriented Frontend Developer, lifelong learner, and adventurer with a professional background in software business strategy. I enjoy development because of the satisfaction I get by overcoming challenges. I am motivated by the opportunity that software provides to positively impact the life of an individual, and the world as a whole. bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+  title: "I’m Yosef Degarge",
+  animated: {
+    first: "I love coding",
+    second: "I code Awsome websites",
+    third: "I am Frontend Developer",
+  },
+  description:
+    "I am a solution oriented Frontend Developer, lifelong learner, and adventurer with a professional background in software business strategy. I enjoy development because of the satisfaction I get by overcoming challenges. I am motivated by the opportunity that software provides to positively impact the life of an individual, and the world as a whole. bibendum",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
-    title: "about my self",
-    aboutme: "   To pursue a challenging career and be part of a progressive organization that gives a scope to enhance my knowledge and utilizing my skills towards the growth of the organization. Highly dedicated and hardworking individual with good interpersonal, communication, and problem-solving skill. Looking for an opportunity in a challenging environment, which will help me in developing new and existing skills while strengthening those I already possess. Energetic and enthusiastic person who is always ready to learn more by working on challenging assignments. A strong communicator able to work in different situations under various circumstances and conditions ",
+  title: "about my self",
+  aboutme:
+    "   To pursue a challenging career and be part of a progressive organization that gives a scope to enhance my knowledge and utilizing my skills towards the growth of the organization. Highly dedicated and hardworking individual with good interpersonal, communication, and problem-solving skill. Looking for an opportunity in a challenging environment, which will help me in developing new and existing skills while strengthening those I already possess. Energetic and enthusiastic person who is always ready to learn more by working on challenging assignments. A strong communicator able to work in different situations under various circumstances and conditions ",
 };
-const worktimeline = [{
-        jobtitle: "Mobile App Developer",
-        where: "Reyness Enginnering",
-        date: "2020",
-    },
-    {
-        jobtitle: "Frontend Developer",
-        where: "Fejlesztő Technology",
-        date: "since 2018",
-    },
-    
+const worktimeline = [
+  {
+    jobtitle: "Mobile App Developer",
+    where: "Reyness Enginnering",
+    date: "2020",
+  },
+  {
+    jobtitle: "Frontend Developer",
+    where: "Fejlesztő Technology",
+    date: "since 2018",
+  },
 ];
 
-const skills = [{
-        name: "PHP",
-        value: 90,
-    },
-    {
-        name: "React Js",
-        value: 95,
-    },
-    {
-        name: "Javascript",
-        value: 80,
-    },
-    {
-        name: "React Js 16.8++",
-        value: 60,
-    },
-    {
-        name: "Jquery",
-        value: 85,
-    },
+const skills = [
+  {
+    name: "PHP",
+    value: 90,
+  },
+  {
+    name: "React Js",
+    value: 95,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "React Js 16.8++",
+    value: 60,
+  },
+  {
+    name: "Jquery",
+    value: 85,
+  },
 ];
 
-const services = [{
-        title: "UI & UX Design",
-        description: "i can build you awsome Design For your comany",
-    },
-    {
-        title: "Mobile Apps",
-        description: "I can build cross Plateform Mobile App using Flutter",
-    },
-    
-       
+const services = [
+  {
+    title: "UI & UX Design",
+    description:
+      " As a UI Designer, I create visually appealing and user-friendly interfaces for digital products using design tools like Sketch, Figma, or Adobe XD. I collaborate with UX designers, developers, and product managers to ensure design solutions meet user needs and business goals. I conduct user research and usability testing to validate design decisions and stay up-to-date with the latest design trends and best practices. As a UX Designer, I focus on creating seamless and enjoyable experiences for users when interacting with digital products. I conduct user research to identify needs and behaviors and create personas, user journeys, and information architecture to guide the design process. I design intuitive and efficient user interfaces in collaboration with UI designers, developers, and product managers. I conduct usability testing to validate design decisions and identify areas for improvement to enhance the overall user experience",
+  },
+  {
+    title: "Mobile Apps",
+    description: "As a Flutter Developer, I specialize in designing, developing, and deploying mobile applications for both Android and iOS platforms using the Flutter framework and Dart programming language. I collaborate with other developers, designers, and product managers to ensure that the app meets user needs and business goals. I also perform testing and debugging to ensure the app functions smoothly and meets quality standards. I stay up-to-date with the latest Flutter development trends and technologies to deliver efficient and high-performing mobile apps.",
+  },
 ];
 
-const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/800/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
 
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "yosefdegaregenakachew@gmail.com",
-    YOUR_FONE: "(2519)00433135",
-    description: "you can find me on my social . let get in touch  ",
-    // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+  YOUR_EMAIL: "yosefdegaregenakachew@gmail.com",
+  YOUR_FONE: "(2519)00433135",
+  description: "you can find me on my social . let get in touch  ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_4yqfosb",
+  YOUR_TEMPLATE_ID: "template_np0etlk",
+  YOUR_USER_ID: "iVj8e_YPnO88Ykj0j",
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
-    youtube: "https://youtube.com",
-    twitch: "https://twitch.com",
+  github: "https://github.com",
+  facebook: "https://facebook.com",
+  linkedin: "https://linkedin.com",
+  twitter: "https://twitter.com",
+  youtube: "https://youtube.com",
+  twitch: "https://twitch.com",
 };
 export {
-    meta,
-    dataabout,
-    dataportfolio,
-    worktimeline,
-    skills,
-    services,
-    introdata,
-    contactConfig,
-    socialprofils,
-    logotext,
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
 };
